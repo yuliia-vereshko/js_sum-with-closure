6f40d3052356be05e9f0bf343a14a24aace952d9
Yuliia Vereshko <pyliatko@gmail.com>
2023-02-26 17:04:18 +0100

    robotAccountant

--- src/makeRobotAccountant.js
+++ src/makeRobotAccountant.js
@@ -6,7 +6,32 @@
  */
 
 function makeRobotAccountant() {
-  // write code here
+  let count = 0;
+
+  return (x) => {
+    let sum = 0;
+
+    return (y) => {
+      const errorMessage = 'Bzzz... Error!';
+
+      if (count < 3) {
+        count++;
+        sum += x + y;
+
+        return sum;
+      } else if (count % 2 !== 0) {
+        count++;
+        sum = 0;
+
+        return errorMessage;
+      } else {
+        count++;
+        sum += x + y;
+
+        return sum;
+      };
+    };
+  };
 }
 
 module.exports = makeRobotAccountant;
